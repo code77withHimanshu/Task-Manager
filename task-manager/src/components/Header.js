cbda8528d7f510226f843dd0ffb240cb684178b1
Himanshu <hsinghi3@gmail.com>
2024-08-28 21:23:40 +0530

    Added Profile Avatars

--- task-manager/src/components/Header.js
+++ task-manager/src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Typography } from '@mui/material';
+import { AppBar, Toolbar, Button, Typography, Avatar } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
 
 function Header() {
   const { user } = useAuth();
+
+  // Function to get the user's initials
+  const getUserInitials = (name) => {
+    if (!name) return '';
+    const names = name.split(' ');
+    const initials = names.map(n => n[0]).join('');
+    return initials.toUpperCase();
+  };
 
   return (
     <AppBar position="static">
@@ -18,14 +26,21 @@ function Header() {
           Task Manager
         </Typography>
         {user ? (
-          <Button 
-            component={Link} 
-            to="/logout" 
-            color="inherit" 
-            style={{ marginLeft: 'auto' }}
-          >
-            Logout
-          </Button>
+          <>
+            <Avatar 
+              style={{ marginLeft: 'auto', marginRight: '10px' }}
+            >
+              {getUserInitials(user.name)}
+            </Avatar>
+            <Button 
+              component={Link} 
+              to="/logout" 
+              color="inherit" 
+              style={{ marginLeft: '10px' }}
+            >
+              Logout
+            </Button>
+          </>
         ) : (
           <>
             <Button 
